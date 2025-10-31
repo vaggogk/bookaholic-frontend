201259583bb486726ac421a6717e95493e174502
vaggogk <vaggogk@gmail.com>
2025-10-31 15:10:32 +0200

    feat:

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -17,7 +17,7 @@ const HomePage = () => {
                 />
             </div>
 
-            {/* Navigation Links */}
+
 
 
             {/* Main Content */}
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -5,13 +5,35 @@ import {Link, useNavigate} from "react-router";
 const LoginPage = () => {
     const navigate = useNavigate();
 
-    const handleSubmit = (e:React.FormEvent) => {
+    const handleSubmit = async (e:React.FormEvent) => {
         e.preventDefault();
 
-        console.log("Logging in");
+        const formData = new FormData(e.currentTarget as HTMLFormElement);
+        const loginData = {
+            username: formData.get('username') as string,
+            password: formData.get('password') as string
+        };
 
-        navigate("/")
+        try {
+            const response = await fetch('http://localhost:8080/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData)
+            });
+
+            if (response.ok) {
+                const userData = await response.json();
+                console.log('Login successful:', userData);
+                navigate("/");
+            }
+        } catch (error) {
+            console.error('Login failed:', error);
+        }
+
     }
+
 
     return (
         <div className="login-container">
@@ -21,12 +43,14 @@ const LoginPage = () => {
                     <FontAwesomeIcon className="icon" icon={faCircleUser} size="10x" style={{color: "#7E4C27",}} />
                     </div>
                     <label htmlFor="username"><FontAwesomeIcon icon={faUser} style={{color: "#7E4C27",}}/>|Username</label>
-                    <input type="text" id="username" />
+                    <input type="text" id="username" name="username"
+                           required/>
                 </div>
                 <div className="form-group" >
 
                     <label htmlFor="password" ><FontAwesomeIcon icon={faLock} style={{color: "#7E4C27",}} />|Password</label>
-                    <input type="password" id="password" />
+                    <input type="password" id="password" name="password"
+                           required />
                 </div>
 
                 <button type="submit" className="login-btn">
