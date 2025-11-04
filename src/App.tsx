fc45af871e8ef1cdaf1d3a53284976d09bb5f3b9
vaggogk <vaggogk@gmail.com>
2025-11-04 19:47:28 +0200

    fix: organized the paths

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,21 @@ import CurrentlyReadingPage from "./components/pages/CurrentlyReadingPage.tsx";
 import ToReadPage from "./components/pages/ToReadPage.tsx";
 import FinishedPage from "./components/pages/FinishedPage.tsx";
 import EditPage from "./components/pages/EditPage.tsx";
-import GaveUp from "../GaveUp.tsx";
+import GaveUp from "./components/pages/GaveUp.tsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-
                 <Route index element={<LoginPage />}/>
                 <Route path="login" element={<LoginPage />}/>
-                <Route path="home_page" element={<HomePage />}/>
                 {/*<Route index element={<HomePage />}/>*/}
+                <Route path="home_page" element={<HomePage />}/>
                 <Route path="library" element={<LibraryPage />}/>
                 <Route path="currently_reading" element={<CurrentlyReadingPage />}/>
                 <Route path="to_read" element={<ToReadPage />}/>
-                <Route path="gave_up" element={<GaveUp />}/>
                 <Route path="finished" element={<FinishedPage />}/>
+                <Route path="gave_up" element={<GaveUp />}/>
                 <Route path="add_book" element={<AddPage />}/>
                 <Route path="edit/:id" element={<EditPage />}/>
                 <Route path="log_out" element={<LoginPage/>}/>
