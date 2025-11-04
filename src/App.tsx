6c2910aca4dbe8152c19892bdbf72530324af866
vaggogk <vaggogk@gmail.com>
2025-11-04 19:28:26 +0200

    feat:add GaveUp path

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import CurrentlyReadingPage from "./components/pages/CurrentlyReadingPage.tsx";
 import ToReadPage from "./components/pages/ToReadPage.tsx";
 import FinishedPage from "./components/pages/FinishedPage.tsx";
 import EditPage from "./components/pages/EditPage.tsx";
+import GaveUp from "../GaveUp.tsx";
 
 function App() {
     return (
@@ -17,17 +18,18 @@ function App() {
 
                 <Route index element={<LoginPage />}/>
                 <Route path="login" element={<LoginPage />}/>
+                <Route path="home_page" element={<HomePage />}/>
                 {/*<Route index element={<HomePage />}/>*/}
-                <Route path="add_book" element={<AddPage />}/>
-                <Route path="log_out" element={<LoginPage/>}/>
-                <Route path="register_page" element={<RegisterPage/>}/>
                 <Route path="library" element={<LibraryPage />}/>
                 <Route path="currently_reading" element={<CurrentlyReadingPage />}/>
                 <Route path="to_read" element={<ToReadPage />}/>
+                <Route path="gave_up" element={<GaveUp />}/>
                 <Route path="finished" element={<FinishedPage />}/>
-                {/*<Route path="go-back" element={<HomePage />}/>*/}
+                <Route path="add_book" element={<AddPage />}/>
                 <Route path="edit/:id" element={<EditPage />}/>
-                <Route path="home_page" element={<HomePage />}/>
+                <Route path="log_out" element={<LoginPage/>}/>
+                <Route path="register_page" element={<RegisterPage/>}/>
+                {/*<Route path="go-back" element={<HomePage />}/>*/}
             </Routes>
         </BrowserRouter>
     )
